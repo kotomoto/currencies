19c4184b7dd9500170e2574200694fde26d51492
kotomoto <jkotor@gmail.com>
2024-04-07 23:43:50 +0200

    Add bottom navigation bar

--- app/src/main/java/com/kotomoto/currencies/MainActivity.kt
+++ app/src/main/java/com/kotomoto/currencies/MainActivity.kt
@@ -6,9 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kotomoto.currencies.ui.theme.CurrenciesTheme
@@ -19,7 +35,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CurrenciesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
+                        badgeCount = 4,
+                    ),
+                    BottomNavigationItem(
+                        title = "My Currencies",
+                        selectedIcon = Icons.Filled.Star,
+                        unselectedIcon = Icons.Outlined.Star,
+                        hasNews = true,
+                    ),
+                    BottomNavigationItem(
+                        title = "Converter",
+                        selectedIcon = Icons.Filled.Refresh,
+                        unselectedIcon = Icons.Outlined.Refresh,
+                        hasNews = false,
+                    )
+                )
+
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = index == selectedItemIndex,
+                                    onClick = {
+                                        selectedItemIndex = index
+                                        // When adding navigation, add something like:
+                                        // navController.navigate(item.route)
+                                    },
+                                    label = {
+                                        Text(text = item.title)
+                                    },
+                                    icon = {
+                                        BadgedBox(badge = {
+                                            if (item.badgeCount != null) {
+                                                Badge {
+                                                    Text(text = item.badgeCount.toString())
+                                                }
+                                            } else if (item.hasNews) {
+                                                Badge()
+                                            }
+                                        }) {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) {
+                                                    item.selectedIcon
+                                                } else {
+                                                    item.unselectedIcon
+                                                },
+                                                contentDescription = item.title,
+                                            )
+                                        }
+                                    })
+                            }
+                        }
+                    },
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
